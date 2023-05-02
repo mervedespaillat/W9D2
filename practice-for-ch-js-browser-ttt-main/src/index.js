180c142c480da3bb9bca11e658c5cdcc4db29080
harjitsingh3 <singh.harjit09@gmail.com>
2023-05-02 16:18:46 -0400

    Set Up Board

--- practice-for-ch-js-browser-ttt-main/src/index.js
+++ practice-for-ch-js-browser-ttt-main/src/index.js
@@ -2,7 +2,7 @@ import Game from "../ttt_node/game.js"
 import View from "./ttt-view.js"
 
 document.addEventListener("DOMContentLoaded", () => {
-  // Your code here
+  const game = new Game();  // has our game's logic
+  const element = document.querySelector(".ttt"); // this element is the visual for our game
+  const view = new View(game, element);
 });
-
-console.log("Hello")
--- practice-for-ch-js-browser-ttt-main/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt-main/src/ttt-view.js
@@ -1,8 +1,23 @@
 class View {
   constructor(game, el) {
+    this.game = game;
+    this.el = el;
+    this.setupBoard();
   }
   
   setupBoard() {
+    const unorderedList = document.createElement("ul")
+    //const listItem = document.createElement("li")
+    const tttNode = document.querySelector(".ttt");
+    
+    for (let i = 0; i < 3; i++) {
+      tttNode.appendChild(unorderedList);
+      for (let j = 0; j < 3; j++) {
+        let listItem = document.createElement("li")
+        unorderedList.appendChild(listItem);
+      }
+    }
+  
   }
   
   handleClick(e) {
