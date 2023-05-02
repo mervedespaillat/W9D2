1f19743bc99bf7eda351d4748ee97af2d7fe1cc7
Merve Dogan-Espaillat <1mervedogan@gmail.com>
2023-05-02 17:39:00 -0400

    almost done

--- practice-for-ch-js-browser-ttt-main/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt-main/src/ttt-view.js
@@ -4,30 +4,43 @@ class View {
     this.el = el;
     this.setupBoard();
   }
-  
+
   setupBoard() {
-    const unorderedList = document.createElement("ul")
+    const unorderedList = document.createElement("ul");
+    unorderedList.className = "row";
     //const listItem = document.createElement("li")
-    const tttNode = document.querySelector(".ttt");
-    
+    this.el.appendChild(unorderedList);
+
     for (let i = 0; i < 3; i++) {
-      tttNode.appendChild(unorderedList);
       for (let j = 0; j < 3; j++) {
-        let listItem = document.createElement("li")
+        let listItem = document.createElement("li");
+        //save the position i,j
+        listItem.dataset.pos = JSON.stringify([i,j])
+        listItem.className = "col";
         unorderedList.appendChild(listItem);
+        listItem.addEventListener( "click", (event) =>{
+          event.target.style.backgroundColor = "white"
+        } )
       }
     }
-  
+
   }
-  
+
   handleClick(e) {
+    let ele = e.target // this is the li itself 
+    let pos = JSON.parse(ele.dataset.pos)   // this suppose to bring the position in the li
+    
+    Game.prototype.playMove(pos)
+
+
+
   }
 
   makeMove(square) {
+
   }
-  
-  handleGameOver() {
-  }
+
+  handleGameOver() {}
 }
 
 export default View;
